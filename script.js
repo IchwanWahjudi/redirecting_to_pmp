8b2300d1bf7dff940418d149e5d69e5ce0656ef9
IchwanWahjudi <ichwan.wahyoudi@gmail.com>
2023-06-10 11:27:31 +0700

    redirecting page

---
+++ script.js
@@ -0,0 +1,7 @@
+let counter = document.querySelector(".main h2");
+let count = 3;
+setInterval(() => {
+	counter.innerText = count;
+	count--;
+	if (count < 1) location.replace("https://pratamamajuperkasa.co.id/");
+}, 1000);
